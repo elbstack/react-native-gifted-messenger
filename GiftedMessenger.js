b845b4b69bb561a8fbc66bf247f10c127aba9f98
Christian Eichinger <christianeichinger@MacBook-Pro-3.fritz.box>
2016-02-11 00:22:57 +0100

    updated scrollTo statements to be compatible with rn-0.19

--- GiftedMessenger.js
+++ GiftedMessenger.js
@@ -256,14 +256,14 @@ var GiftedMessenger = React.createClass({
   scrollToBottom() {
     if (this.listHeight && this.footerY && this.footerY > this.listHeight) {
       var scrollDistance = this.listHeight - this.footerY;
-      this.scrollResponder.scrollTo(-scrollDistance);
+      this.scrollResponder.scrollTo({y: -scrollDistance});
     }
   },
 
   scrollWithoutAnimationToBottom() {
     if (this.listHeight && this.footerY && this.footerY > this.listHeight) {
       var scrollDistance = this.listHeight - this.footerY;
-      this.scrollResponder.scrollWithoutAnimationTo(-scrollDistance);
+      this.scrollResponder.scrollTo({y: -scrollDistance});
     }
   },
   
